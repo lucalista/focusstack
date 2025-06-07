30336bb45a8ce2173a59fd85cfa0dbfe7f431bf4
Luca Lista <luca.lista@infn.it>
2025-06-07 14:02:37 +0200

    update

--- sandbox/log_gui.py
+++ sandbox/log_gui.py
@@ -27,20 +27,30 @@ class QtLogFormatter(logging.Formatter):
         color = self.COLORS.get(record.levelname, '')
         fmt = f"[blue3][[{color}]%(levelname).3s[/] %(asctime)s] %(message)s[/]"  # noqa
         return self.ANSI_ESCAPE.sub('', logging.Formatter(fmt).format(record).replace("\r", "").rstrip())
-        
 
-class QTextEditLogger(QTextEdit, logging.StreamHandler):
-    def __init__(self, parent=None):
-        QTextEdit.__init__(self, parent)
-        logging.StreamHandler.__init__(self)
-        layout = QVBoxLayout()
-        self.setLayout(layout)
+
+class QRichHandler(RichHandler):
+    def __init__(self, text_edit):
         self.console = Console(file=open(os.devnull, "wt"), record=True, width=100, height=20, highlight=False,
                                soft_wrap=False, color_system="truecolor", tab_size=4)
-        self.rich_handler = RichHandler(show_time=False, show_path=False, show_level=False, markup=True,
-                                        console=self.console)
-        self.rich_handler.setFormatter(QtLogFormatter())
+        RichHandler.__init__(self, show_time=False, show_path=False, show_level=False, markup=True,
+                             console=self.console)
         self.setFormatter(QtLogFormatter())
+        self.text_edit = text_edit
+
+    def emit(self, record) -> None:
+        RichHandler.emit(self, record)
+        indent_width = 11*self.text_edit.fontMetrics().averageCharWidth()
+        html_template = f'<p style="background-color: {{background}}; color: {{foreground}}; margin: 0; margin-left:{indent_width}px; text-indent:-{indent_width}px;white-space: pre-wrap"><code>{{code}}</code></p>'
+        html = self.console.export_html(clear=True, code_format=html_template, inline_styles=True)
+        self.text_edit.emit_html(html)
+
+
+class QTextEditLogger(QTextEdit):
+    def __init__(self, parent=None):
+        QTextEdit.__init__(self, parent)
+        layout = QVBoxLayout()
+        self.setLayout(layout)
         self.setWordWrapMode(QTextOption.WrapMode.WordWrap)
         self.setAcceptRichText(True)
         self.setReadOnly(True)
@@ -48,14 +58,7 @@ class QTextEditLogger(QTextEdit, logging.StreamHandler):
         font.setStyleHint(QFont.StyleHint.TypeWriter)
         self.setFont(font)
 
-    def setLevel(self, level):
-        self.rich_handler.setLevel(self.level)
-
-    def emit(self, record) -> None:
-        self.rich_handler.emit(record)
-        indent_width = 11*self.fontMetrics().averageCharWidth()
-        html_template = f'<p style="background-color: {{background}}; color: {{foreground}}; margin: 0; margin-left:{indent_width}px; text-indent:-{indent_width}px;white-space: pre-wrap"><code>{{code}}</code></p>'
-        html = self.console.export_html(clear=True, code_format=html_template, inline_styles=True)
+    def emit_html(self, html):
         pattern = r'<span style="color: #00ff00; text-decoration-color: #00ff00; font-weight: bold">(\d{2}:\d{2}:\d{2})</span>'
         replacement = r'<span style="color: #008080; text-decoration-color: #008080; font-weight: bold">\1</span>'
         html = re.sub(pattern, replacement, re.sub(r'\s+[\n]', '\n', html))
@@ -65,7 +68,7 @@ class QTextEditLogger(QTextEdit, logging.StreamHandler):
         c.movePosition(QTextCursor.MoveOperation.End)
         self.setTextCursor(c)
         QApplication.processEvents()
-
+        
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -91,8 +94,9 @@ class MainWindow(QMainWindow):
         self.button_l.setEnabled(False)
         logger = logging.getLogger()
         logger.setLevel(logging.DEBUG)
-        logger.addHandler(self.text_edit)
-        self.text_edit.setLevel(logging.DEBUG)
+        self.handler = QRichHandler(self.text_edit)
+        self.handler.setLevel(logging.DEBUG)
+        logger.addHandler(self.handler)
         self.log_worker = LogWorker()
         self.log_worker.log_signal.connect(self.handle_log_message)
         self.log_worker.html_signal.connect(self.handle_html_message)
@@ -135,7 +139,6 @@ class LogWorker(QThread):
             self.log_signal.emit("CRITICAL", "Crash!!!")
             sleep(0.1)
         self.end_signal.emit(1)
-
 
         
 if __name__ == "__main__":
